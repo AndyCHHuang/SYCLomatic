64ab47751fc65cfb5c9fd923d5d15fd41df1479a
Ni, Wenhui <wenhui.ni@intel.com>
2023-10-23 22:38:32 +0800

    enable Var/Type/Field Schema
    Signed-off-by: Ni, Wenhui <wenhui.ni@intel.com>

--- clang/lib/DPCT/ASTTraversal.cpp
+++ clang/lib/DPCT/ASTTraversal.cpp
@@ -24,6 +24,7 @@
 #include "NCCLAPIMigration.h"
 #include "OptimizeMigration.h"
 #include "SaveNewFiles.h"
+#include "TestMigration.h"
 #include "TextModification.h"
 #include "ThrustAPIMigration.h"
 #include "Utility.h"
@@ -14338,6 +14339,8 @@ REGISTER_RULE(ThrustTypeRule, PassKind::PK_Migration, RuleGroupKind::RK_Thrust)
 REGISTER_RULE(WMMARule, PassKind::PK_Analysis)
 
 REGISTER_RULE(ForLoopUnrollRule, PassKind::PK_Migration)
+
+REGISTER_RULE(TESTRule, PassKind::PK_Migration)
 
 void ComplexAPIRule::registerMatcher(ast_matchers::MatchFinder &MF) {
   auto ComplexAPI = [&]() {
